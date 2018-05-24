63f2ffbaf2027614c4b9db303eace123d81e04dc
飞 <345364004@qq.com>
2018-05-24 15:00:10 +0800

    session

---
+++ models/session.go
@@ -0,0 +1,110 @@
+package models
+
+import (
+	"github.com/garyburd/redigo/redis"
+	log "github.com/alecthomas/log4go"
+)
+
+type Session struct {
+	sessid string
+
+}
+
+
+func NewSession(sessid ...string)*Session{
+	if len(sessid) > 0{
+		return &Session{
+			sessid:sessid[0],
+		}
+	}else{
+		return &Session{
+			sessid:generateSessid(),
+		}
+	}
+}
+
+
+/**
+	设置session过期时间
+ */
+func (this *Session) Expire(){
+	rconn := conn.pool.Get()
+	defer rconn.Close()
+
+	rconn.Do("EXPIRE",this.sessid,3600)
+}
+
+/**
+
+ */
+func (this *Session) SessionID()string{
+	return this.sessid
+}
+/**
+	写入session，以哈希的形式写入redis
+ */
+func (this *Session) SetSession(key string,value interface{}){
+	rconn := conn.pool.Get()
+	defer rconn.Close()
+
+	rconn.Do("HMSET",this.sessid,key,value)
+}
+
+/**
+
+ */
+func (this *Session) GetSession(key string)interface{}{
+	rconn := conn.pool.Get()
+	defer rconn.Close()
+
+	values,err := redis.StringMap(rconn.Do("HGETALL",this.sessid))
+	if err != nil{
+		log.Error("redis.StringMap has error:%v",err)
+		return nil
+	}
+
+	return values[key]
+}
+
+/**
+	判断session是否存在
+ */
+func (this *Session) Has(key string)bool{
+	rconn := conn.pool.Get()
+	defer rconn.Close()
+
+	values,err := redis.StringMap(rconn.Do("HGETALL",this.sessid))
+	if err != nil{
+		log.Error("redis.StringMap has error:%v",err)
+		return false
+	}
+	_,ok := values[key]
+
+	return ok
+}
+
+/**
+	删除session或者session中指定的字段
+ */
+
+func (this *Session) Del(keys ...string){
+	rconn := conn.pool.Get()
+	defer rconn.Close()
+
+	if len(keys) > 0{
+		rconn.Do("HDEL",redis.Args{}.Add(this.sessid).AddFlat(keys)...)
+	}else{
+		rconn.Do("DEL",this.sessid)
+	}
+}
+
+
+
+
+
+
+
+func generateSessid()string{
+	rstr := RandStr(6)
+	return ToMd5(rstr)
+}
