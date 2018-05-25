a2900eb5ddc900252a2e33a0b350fc215fb91934
飞 <345364004@qq.com>
2018-05-25 16:05:28 +0800

    Add files via upload

--- controllers/managercontroller.go
+++ controllers/managercontroller.go
@@ -4,13 +4,20 @@ import (
 	"gopkg.in/gin-gonic/gin.v1"
 	"net/http"
 	"blog/models"
+	"sync"
 )
 
+/**
+	后台首页
+ */
 func MIndex(context *gin.Context){
 
 	context.HTML(http.StatusOK,"manage/index.html",nil)
 }
 
+/**
+	网站设置
+ */
 func Webset(context *gin.Context){
 
 	//网站设置&&个人档案
@@ -19,5 +26,25 @@ func Webset(context *gin.Context){
 
 	context.HTML(http.StatusOK,"manage/webset.html",gin.H{
 		"webSet":webSet,
+	})
+}
+
+/**
+	标签管理
+ */
+func Tag(context *gin.Context){
+	var wg sync.WaitGroup
+	//标签
+	tag_list := models.TagList()
+	tagList := make([]*models.Tag,len(tag_list))
+	for pos,id := range tag_list{
+		wg.Add(1)
+		models.MultipleLoadTag(id,pos,tagList,&wg)
+	}
+
+	wg.Wait()
+
+	context.HTML(http.StatusOK,"manage/webset.html",gin.H{
+		"tagList":tagList,
 	})
 }
