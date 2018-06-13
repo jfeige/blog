7a544fe1994d8197e3387cebd2c48bf9af1ac6c1
飞 <345364004@qq.com>
2018-06-13 10:55:25 +0800

    Add files via upload

--- controllers/logincontroller.go
+++ controllers/logincontroller.go
@@ -2,9 +2,22 @@ package controllers
 
 import (
 	"gopkg.in/gin-gonic/gin.v1"
+	"blog/models"
+	"net/http"
+	"fmt"
 )
 
 
 func Logout(context *gin.Context){
 
+	//销毁session
+	tmpSession,exists := context.Get("session")
+	if exists{
+		fmt.Println("session will be deleted")
+		session := tmpSession.(*models.Session)
+		session.Del()
+	}
+
+	//跳转到登录页面或者前台首页
+	context.Redirect(http.StatusFound,"/")
 }
