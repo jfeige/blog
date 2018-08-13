1c9312ce73d38d2fa6c0511d3934e4cc672b193d
lifei <lifei@95195.com>
2018-08-13 14:36:51 +0800

    add godeps

--- env.sh
+++ env.sh
@@ -4,4 +4,9 @@ ps ax |grep 'blog' | awk '{print $1}' |xargs kill -9
 
 sleep 2
 
-go run /apps/golang/src/blog/blog.go &
+godep go build blog
+
+chmod 777 blog
+
+./blog &
+#go run /apps/golang/src/blog/blog.go &
